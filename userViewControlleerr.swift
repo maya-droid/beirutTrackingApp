c82d3398e1926eec2b5d0b9273d6dbdb6b0d5000
maya-droid <69542364+maya-droid@users.noreply.github.com>
2020-08-14 21:34:59 +0400

    Update userViewControlleerr.swift

--- userViewControlleerr.swift
+++ userViewControlleerr.swift
@@ -1,61 +1,67 @@
-//
-//  userViewControlleerr.swift
-//  BeirutTrackingApp
-//
-//  Created by SPEAKFLUENCE GLOBAL on 10/08/2020.
-//  Copyright © 2020 Maya Bridgman. All rights reserved.
-//
 
 import UIKit
 import FirebaseDatabase
 import FirebaseStorage
 import FirebaseAuth
 
-class userViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    @IBOutlet var tableView: UITableView!
-
-    var profileData: [String] = []
+class userTableViewController: UITableViewController {
+    
+    var profileData = [profileStruct]()
     let storageRef = Storage.storage().reference()
     var databaseRef = Database.database().reference()
     var ref: DatabaseReference?
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        ref = Database.database().reference()
-        print(ref)
-        if ref == nil {
-            print("ref is nil")
-        }
-        //let userID = Auth.auth().currentUser?.uid
-        ref?.child("users").observeSingleEvent(of: .value, with: { (snapshot) in
-            print("observeSingleEvent callback closure called")
-            print(snapshot.value)
-            let profile = snapshot.value as? String
-            print(self.ref?.child("users"))
-            if let actualProfile = profile {
-                print("profile is not nil")
-                self.profileData.append(actualProfile)
-                print(self.profileData)
-                DispatchQueue.main.async {
-                    self.tableView.reloadData()
-            
-                }
-            } else { print("profile is not nil") }
-        }) { (error) in
-            print(error.localizedDescription)
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print("Calculating number of rows in tableview: \(profileData.count)")
-        return profileData.count
-    }
 
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        print("Getting cell at index: \(indexPath.row)")
-        print("Cell text: \(self.profileData[indexPath.row])")
-        let cell = tableView.dequeueReusableCell(withIdentifier: "firstName_cell", for: indexPath)
-        cell.textLabel?.text = self.profileData[indexPath.row]
-        return cell
+     
+     override func viewDidLoad() {
+         super.viewDidLoad()
+         getProfile()
+         // Do any additional setup after loading the view.
+         tableView.register(ProfileCell.self, forCellReuseIdentifier: "ProfileCell")
+     }
+
+     override func didReceiveMemoryWarning() {
+         super.didReceiveMemoryWarning()
+         // Dispose of any resources that can be recreated.
+     }
+     
+     
+     struct profileStruct {
+         let email : String!
+         let phoneNumber : String!
+     }
+     
+    
+    func getProfile() {
+        let databaseRef = Database.database().reference()
+        databaseRef.child("users").queryOrderedByKey().observeSingleEvent(of: .childAdded, with: {
+                    snapshot in
+                    let email = (snapshot.value as? NSDictionary)!["email"] as? String
+                    let phoneNumber = (snapshot.value as? NSDictionary
+                        )!["phone number"] as? String
+                    self.profileData.append(profileStruct(email: email, phoneNumber: phoneNumber))
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
+                })
     }
+    
+
+
+     override func numberOfSections(in tableView: UITableView) -> Int {
+         return 1
+     }
+     
+     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         return profileData.count
+     }
+     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         guard let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileCell") as? ProfileCell else { return UITableViewCell() }
+         cell.emailLabel?.text = profileData[indexPath.row].email
+         print(profileData[indexPath.row].email)
+         return cell
+     }
 }
+
+
+
