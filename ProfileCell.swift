21d85a61ad0aeeb491813eba9ab4608b009a188d
maya-droid <69542364+maya-droid@users.noreply.github.com>
2020-08-14 21:35:55 +0400

    Add files via upload

---
+++ ProfileCell.swift
@@ -0,0 +1,26 @@
+//
+//  ProfileCell.swift
+//  BeirutTrackingApp
+//
+//  Created by SPEAKFLUENCE GLOBAL on 14/08/2020.
+//  Copyright © 2020 Maya Bridgman. All rights reserved.
+//
+
+import UIKit
+
+class ProfileCell: UITableViewCell {
+
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
